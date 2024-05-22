7088aeeb8ec1b1a37976ad099352892d1bababbb
Rodrigo <rodrigonetogithub@gmail.com>
2024-05-22 20:27:06 -0300

    refactor: componente add funcionabildades e botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 
@@ -8,6 +9,11 @@ function App() {
   return (
     <>
     <Header/>
+    {/* <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Header/>}/>
+    </Routes>
+    </BrowserRouter> */}
     </>
   )
 }
